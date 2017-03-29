559385776030147b859f7c5391c6005713dfc30c
Tamal <tamal.mondal@appsbee.com>
2017-03-29 10:01:06 +0530

    app service

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
 // set the view engine to ejs
 app.set('view engine', 'ejs')
@@ -8,6 +8,6 @@ app.get('/', function(req, res) {
     res.render('index');
 })
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!')
+app.listen((process.env.PORT || 5000), function() {
+    console.log(`Example app listening on port ${(process.env.PORT || 5000)}!`);
 })
