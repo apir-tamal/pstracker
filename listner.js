7ee038a0b00f92b3ba03a8c4f615f95a8a8b2fb2
Tamal <tamal.mondal@appsbee.com>
2017-04-03 00:52:32 +0530

    test

--- listner.js
+++ listner.js
@@ -1,24 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 
-app.get('/', function(req, res) {
+app.all('/', function(req, res) {
 
     console.log('Incoming Request!');
-    console.log(req.body);
+    console.log(req);
 
     res.send('Ok');
 
 });
 
-app.post('/', function(req, res) {
-
-    console.log('Incoming Request!');
-    console.log(req.body);
-
-    res.send('Ok');
-
-});
 
 app.listen((process.env.PORT || 8080), function() {
     console.log(`Example app listening on port ${(process.env.PORT || 8080)}!`);
