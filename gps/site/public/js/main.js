28b9813d099306a36065d80e0caefa772fe453f0
Tamal <tamal.mondal@appsbee.com>
2017-04-03 11:10:18 +0530

    test

--- gps/site/public/js/main.js
+++ gps/site/public/js/main.js
@@ -3,116 +3,119 @@ var devices = {}
 
 
 function initialize(latLng) {
-	var mapOptions = {
-		center: latLng,
-		zoom: 11
-	};
-	map = new google.maps.Map(document.getElementById('map-canvas'),
-			mapOptions);
+    var mapOptions = {
+        center: latLng,
+        zoom: 11
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 }
 
 
-google.maps.event.addDomListener(window, 'load', function(){
+google.maps.event.addDomListener(window, 'load', function() {
 
 });
 
-function displayData(data){
-	console.log(data, '-----');
-	$ul = $('<ul>');
-	$.each(data, function(index, value) {
-		$ul.append($('<li>').html("<strong>" + index + "</strong><span>" + value + "</span>"));
-	});
+function displayData(data) {
+    console.log(data);
+    $ul = $('<ul>');
+    $.each(data, function(index, value) {
+        $ul.append($('<li>').html("<strong>" + index + "</strong><span>" + value + "</span>"));
+    });
 
-	$(".data").html($ul);
+    $(".data").html($ul);
 }
 
-function getDevice(uid){
-	return devices[uid] || null;
+function getDevice(uid) {
+    return devices[uid] || null;
 }
 
-function checkIfMapExists(latLng){
-	if(!map) {
-		initialize(latLng);
-	}
+function checkIfMapExists(latLng) {
+    if (!map) {
+        initialize(latLng);
+    }
 }
 
-function addDevice(uid, latLng){
-	checkIfMapExists(latLng);
+function addDevice(uid, latLng) {
+    checkIfMapExists(latLng);
 
-	var marker = new google.maps.Marker({
-		position: latLng,
-		map: map,
-		animation: google.maps.Animation.DROP,
-		title: 'UID: ' + uid,
-		uid: uid
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: 'UID: ' + uid,
+        uid: uid
 
-	});
+    });
 
-	marker.setMap(map);
+    marker.setMap(map);
 
-	google.maps.event.addListener(marker, 'click', function(){
-		var deviceData = getDevice(this.uid).lastData;
-		console.log(deviceData);
+    google.maps.event.addListener(marker, 'click', function() {
+        var deviceData = getDevice(this.uid).lastData;
+        console.log(deviceData);
 
-		displayData(deviceData);
-	});
+        displayData(deviceData);
+    });
 
-	devices[uid] = {
-		uid: uid,
-		marker: marker,
-		path: null
-	}
+    devices[uid] = {
+        uid: uid,
+        marker: marker,
+        path: null
+    }
 
-	return devices[uid];
+    return devices[uid];
 }
 
-function checkIfDeviceExists(data){
-	var uid = data.uid;
+function checkIfDeviceExists(data) {
+    var uid = data.uid;
 
-	if(typeof devices[uid] == "undefined") {
-		return addDevice(uid, new google.maps.LatLng(data.latitude, data.longitude));
-	}
+    if (typeof devices[uid] == "undefined") {
+        return addDevice(uid, new google.maps.LatLng(data.latitude, data.longitude));
+    }
 
-	return devices[uid];
+    return devices[uid];
 }
 
 var socket = io.connect(window.location.host);
-socket.on('ping', function (data) {
-	console.log(data);
-	var device = checkIfDeviceExists(data);
-	device.lastData = data;
+socket.on('ping', function(data) {
+    if (!!data) {
+        console.log(data);
+        var device = checkIfDeviceExists(data);
+        device.lastData = data;
 
-	var position = new google.maps.LatLng(data.latitude, data.longitude);
+        var position = new google.maps.LatLng(data.latitude, data.longitude);
 
-	device.path.getPath().push(position);
-	device.marker.setPosition(position);
+        device.path.getPath().push(position);
+        device.marker.setPosition(position);
 
-	socket.emit('my other event', { my: 'data' });
+        socket.emit('my other event', {
+            my: 'data'
+        });
+    }
 });
 
-socket.on('positions', function (data) {
-	var device;
-	console.log(data.positions);
-	data.positions = data.positions.reverse();
-	$.each(data.positions, function(index, value) {
-		device = checkIfDeviceExists(value);
-		var position = new google.maps.LatLng(value.latitude, value.longitude);
+socket.on('positions', function(data) {
+    var device;
+    console.log(data.positions);
+    data.positions = data.positions.reverse();
+    $.each(data.positions, function(index, value) {
+        device = checkIfDeviceExists(value);
+        var position = new google.maps.LatLng(value.latitude, value.longitude);
 
-		if(!device.path)
-		{
-			device.path = new google.maps.Polyline({
-				path: [],
-				geodesic: true,
-				strokeColor: '#FF0000',
-				strokeOpacity: 1.0,
-				strokeWeight: 2
-		  	});
-		  	device.path.setMap(map);
-		}
+        if (!device.path) {
+            device.path = new google.maps.Polyline({
+                path: [],
+                geodesic: true,
+                strokeColor: '#FF0000',
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+            });
+            device.path.setMap(map);
+        }
 
-		device.marker.position = position;
+        device.marker.position = position;
 
-		device.path.getPath().push(position);
-	})
+        device.path.getPath().push(position);
+    })
 
 });
